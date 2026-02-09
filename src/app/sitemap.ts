d6b9ff17f86d96e595172aa790cab8c854408db5
1203992808@qq.com <1203992808@qq.com>
2026-02-10 00:46:54 +0800

    更新时间

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,13 +4,20 @@ import { envConfigs } from '@/config';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const appUrl = envConfigs.app_url;
+  const lastModified = '2026-02-10T00:43:00Z';
 
   return [
     {
       url: appUrl,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
+      lastModified,
+      changeFrequency: 'daily',
       priority: 1,
+    },
+    {
+      url: `${appUrl}/create`,
+      lastModified,
+      changeFrequency: 'daily',
+      priority: 0.9,
     },
   ];
 }
